da10ab889f0e87b2f32fad51f49bde32d4270532
Markiyan Pyts <markiyan.pyts@osf-global.com>
2016-06-02 02:18:00 +0300

    fixed the functionality

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 This is configured gulp file and npm packages set which will optimize the size of all images under the directory to which it's extracted to.
 ##Installation
 1) Install [node.js](https://nodejs.org/en)
-2) Run `npm install gulp-directory-images-optimizer` under the directory under which you want to optimize images.
-3) Run `npm run optimize` or just `gulp` under the same directory.
+2) Clone this repo to the directory under which you want to optimize images.
+3) Run `npm install` under the same directory.
+4) Run `npm run imgmin`
 
-Step 3 Will optimize images in the root of directory and under each subdirectory.
+
+Step 4 Will optimize images in the root of directory and under each subdirectory.
 
 This gulp app is powered by https://www.npmjs.com/package/gulp-imagemin and it's plugins.
 
--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,25 @@ var gulp              = require("gulp"),
     mergeStream       = require("merge-stream"),
     imagemin          = require("gulp-imagemin"),
     imageminJpegoptim = require('imagemin-jpegoptim'),
-    imageminPngquant  = require('imagemin-pngquant');
+    imageminPngquant  = require('imagemin-pngquant'),
+    path = require("path");
 
 /* Images paths */
 var IMAGES_PATHS = [
         {
-            "src" : "*/**/*.jpg",
+            "src" : path.resolve(process.env.INIT_CWD + "/**/*.jpg"),
+            "dst" : ".",
+        },
+        {
+            "src" : path.resolve(process.env.INIT_CWD + "/**/*.gif"),
+            "dst" : ".",
+        },
+        {
+            "src" : path.resolve(process.env.INIT_CWD + "/**/*.png"),
+            "dst" : ".",
+        },
+        {
+            "src" : path.resolve(process.env.INIT_CWD + "/**/*.svg"),
             "dst" : ".",
         }
     ];
@@ -21,7 +34,7 @@ gulp.task("optimizeImages", function() {
     var streams = mergeStream();
 
     IMAGES_PATHS.forEach(function(p) {
-        //console.log(p.src, )
+        console.log("path",p.src )
         streams.add(gulp.src(p.src)
 		.pipe(imagemin([
             imagemin.gifsicle(),
